b7c0ca0747dd0d2b39426e98487b5e4f7c9ecbff
Highwind245 <joshuahughes1426@gmail.com>
2021-03-04 09:59:42 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+Console.WriteLine("Guess the secret number.");
+Console.WriteLine("------------------------");
+Console.Write(">");
+
+string guess = Console.ReadLine();
+
+Console.Write($"You guessed {guess}");
