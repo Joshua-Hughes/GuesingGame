d286a96290579b14f990501e964d10db8ef1742c
Highwind245 <joshuahughes1426@gmail.com>
2021-03-04 15:12:10 -0600

    Project Done

--- Program.cs
+++ Program.cs
@@ -2,21 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 
-//Correct Number to guess
 int correctNumber = new Random().Next(0, 999);
 
-//Number of guesses the user gets
-int tries = 4;
-
-//Starting prompt
 Console.WriteLine("Guess the secret number.");
 Console.WriteLine("------------------------");
 
-//Logic to handle user input
-for (int i = 1; i <= tries; i++)
+Console.WriteLine("Select a difficulty.");
+Console.WriteLine(@"1. Easy, 2. Difficult, 3. Impossible, 4. Baby Time ");
+
+int difficulty = Int32.Parse(Console.ReadLine());
+
+Dictionary<int, int> difficultySelection = new Dictionary<int, int>() {
+    {1, 10},
+    {2, 5},
+    {3, 1},
+    {4, int.MaxValue}
+};
+
+int levelTries = difficultySelection[difficulty];
+int counter = levelTries;
+
+for (int i = 1; i <= counter; i++)
 {
 
-Console.Write($"Guess {i} out of 4:  ");
+Console.Write($"Guess {i} out of {counter}:  ");
 
 string guess = Console.ReadLine();
 int guessInt;
@@ -27,7 +36,7 @@ Int32.TryParse(guess, out guessInt);
         Console.Write("Congrats! You got it!");
         return;
     }
-    else if (i < tries)
+    else if (i < counter)
     {
         if (guessInt > correctNumber)
         {
