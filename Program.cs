e7edcab87fdcf9a9e4168dd827f8f7c698eea636
Highwind245 <joshuahughes1426@gmail.com>
2021-03-04 10:40:14 -0600

    completed phase 3

--- Program.cs
+++ Program.cs
@@ -4,17 +4,32 @@ using System.Linq;
 
 string correctNumber = "42";
 
+int tries = 4;
+
 Console.WriteLine("Guess the secret number.");
 Console.WriteLine("------------------------");
+
+
+for (int i = 1; i <= tries; i++)
+{
+
 Console.Write(">");
 
 string guess = Console.ReadLine();
 
-if (guess == correctNumber)
-{
-    Console.WriteLine("You guessed correctly!");
+
+    if (guess == correctNumber)
+    {
+        Console.Write("Congrats! You got it!");
+        return;
+    }
+    else if (i < tries)
+    {
+        Console.WriteLine("Incorrect! Try Again!");
+        
+    }
+    else
+    {
+        Console.Write("You failed!");
+    }
 }
-else
-{
-    Console.WriteLine("Incorrect!");
-};
